fa35ed6e84ad8f397708efa69a7dae83fd6ad768
Pedro S. Moreira <pedsmoreira@gmail.com>
2017-06-04 14:17:45 -0300

    Adding initial version with untested engine and some UI

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppView from "app/views/AppView";
+
+import "./index.scss";
+
+ReactDOM.render(
+    <AppView />,
+    document.getElementById('root')
+);
